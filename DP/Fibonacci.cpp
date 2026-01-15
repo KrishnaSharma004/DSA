93070925a06909864aeac51b1bc9ba1c115f0ec6
Krishna Sharma <rakesh7355249204@gmail.com>
2026-01-15 12:15:15 +0530

    Fibonacci

--- DP/Fibonacci.cpp
+++ DP/Fibonacci.cpp
@@ -4,8 +4,8 @@ const int N = 1e5 + 10;
 int dp[N];
 //TOP-DOWN Approach :
 int fib_dp(int n){
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    if(n == 1) return 0;
+    if(n == 2) return 1;
     if(dp[n] != -1) return dp[n];
     return dp[n] = fib_dp(n-1) + fib_dp(n-2); 
 }
